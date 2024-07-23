2d67be29ff1c42592dac14bd9c2dac5251aa66da
mizuki-iijima <zima.web05@gmail.com>
2024-07-23 21:20:30 +0900

    新規作成ボタンコンポーネント追加

---
+++ src/components/CreateBtn/CreateBtn.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+export const CreateBtn = () => {
+    
+    const createMemo = () => {
+
+    }
+
+    return(
+        <Button variant="contained" onClick={createMemo} color="primary">新規作成</Button>
+    );
+}
