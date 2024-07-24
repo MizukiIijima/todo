747ca24b90726513bc79335640eb4601c8bf351c
mizuki-iijima <zima.web05@gmail.com>
2024-07-24 16:38:23 +0900

    main.jsxにThemeProviderを追加

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
+import { ThemeProvider } from '@emotion/react'
+import theme from "./theme.js"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
 )
